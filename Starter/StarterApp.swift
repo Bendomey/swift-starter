a51e21eacf25e681979e9d8fbaa1a5d014d44215
Benjamin Domey <domeybenjamin1@gmail.com>
2021-11-11 00:09:01 +0000

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Domey Benjamin on 11/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
