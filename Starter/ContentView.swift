923cba9b1874f48aeb38aea1d9d9fb387ad11466
Benjamin Domey <domeybenjamin1@gmail.com>
2021-11-11 19:15:28 +0000

    initial commit

--- Starter/ContentView.swift
+++ Starter/ContentView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue,.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
     }
-}
+}t
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
